a174e0ed3d45af024b9be4bba6fdfdcffe3baca3
Mari Topel <maritopel@gmail.com>
2025-06-20 00:07:00 -0500

    added in images that will appear along with the selected pet. edited some text in the reset button for roleplay reasons

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const state = {
   pet: null, //will hold { type, name } once a pet is created
 };
 
+//object that holds the pet images
+const petImages = {
+  dragon: "images/dragon.png",
+  cat: "images/cat.png",
+  dog: "images/dog.png",
+  plant: "images/plant.png",
+  "space octopus": "images/octopus.png",
+};
+
 //function that creates the pet form
 function petForm(onSave) {
   const form = document.createElement("form"); //this creates the pet form
   form.id = "pet-form";
 
+  // makes the form in html
   form.innerHTML = `
     <label for="pet-type">Choose a pet:</label>
     <select id="pet-type" name="petType" required>
@@ -44,12 +54,22 @@ function confirmationView(pet, onReset) {
   summary.textContent = `${pet.name} the ${pet.type}`; //adds the text "pet name the pet type"
 
   const resetButton = document.createElement("button"); //creates a button to be assigned the reset function
-  resetButton.textContent = "Reset Selections"; //adds text to the button
+  resetButton.textContent = "I want to be matched with a different creature."; //adds text to the button
   resetButton.addEventListener("click", onReset); //makes it so when you click the button it performs the onReset function
 
   const header = document.createElement("h2"); // makes a <h2>
   header.textContent = "You have been matched with"; //adds the text to the <h2>
 
+  // makes image of the pet
+  const img = document.createElement("img"); //makes <img>
+  img.src = petImages[pet.type] || ""; //gets pet image based on type that is connect to the image selection or nothing if returns false
+  img.alt = pet.type; // adds alt text to say what the pet type is that is connected to that image
+  img.style.width = "150px";
+  img.style.height = "auto";
+  img.style.display = "block";
+  img.style.margin = "1rem auto";
+
+  container.appendChild(img); //adds image to the container
   container.appendChild(header); //adds header to container
   container.appendChild(summary); //adds summary to container
   container.appendChild(resetButton); //adds resetButton to container
@@ -57,7 +77,7 @@ function confirmationView(pet, onReset) {
   return container;
 }
 
-// root app (will somehow translate to react well)
+// root app (will somehow translate to REACT when I learn REACT)
 function App(root) {
   function savePet(petData) {
     state.pet = petData;
